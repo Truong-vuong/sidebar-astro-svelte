51a004b082514bc6d5900c99aa84cd8be952a005
Vuong Truong <vuong.truong@mgisolutions.com>
2023-08-28 12:03:13 +0700

    icon fix

--- src/components/Icons.svelte
+++ src/components/Icons.svelte
@@ -17,50 +17,24 @@
   export let colorHover: string = ''
 </script>
 
-<div class="icon-box group" style="--backgroundHover:{backgroundHover}; --backgroundColor:{backgroundColor}; --borderRadius:{borderRadius}; --borderRadiusHover:{borderRadiusHover}" 
+<div class="icon-box" style="--backgroundHover:{backgroundHover}; --backgroundColor:{backgroundColor}; --borderRadius:{borderRadius}; --borderRadiusHover:{borderRadiusHover}" 
 style:width={boxWidth} style:height={boxHeight}>
   <svg style='--colorHover:{colorHover};'
   {width}
   {height}
-  class={classList}
-  stroke={iconValue.stroke}
-  stroke-width={iconValue.strokeWidth}
   viewBox={iconValue.viewBox || '0 0 16 16'}
   fill={iconValue.fill || 'none'}
   xmlns="http://www.w3.org/2000/svg"
 >
-  {#if iconValue.g && !!Object.keys(iconValue.g).length}
-    <g style={iconValue.g.style} transform={iconValue.g.transform} id={iconValue.g.id}>
-      {#each iconValue.path as path}
-        <path
-          d={path.d}
-          clip-rule={path.clipRule}
-          fill-rule={path.fillRule}
-          fill={path.fill}
-          id={path.id}
-          transform={path.transform}
-          stroke={path.stroke}
-          stroke-linecap={path.strokeLineCap}
-          stroke-width={path.strokeWidth}
-        ></path>
-      {/each}
-    </g>
-  {:else}
-    {#each iconValue.path as path, i}
-      <path
-        d={path.d}
-        stroke={path.stroke}
-        stroke-linecap={path.strokeLineCap}
-        stroke-linejoin={path.strokeLineJoin}
-        stroke-width={path.strokeWidth}
-        clip-rule={path.clipRule}
-        fill-rule={path.fillRule}
-        fill={fills[i] ?? path.fill}
-      ></path>
-    {/each}
-  {/if}
+  
+  {#each iconValue.path as path, i}
+    <path
+      d={path.d}
+      fill={fills[i] ?? path.fill}
+    ></path>
+  {/each}
   {#if tooltip}
-    <div class="icon-box__tooltip opacity-0 group-hover:opacity-100">
+    <div class="icon-box__tooltip">
       {tooltip}
     </div>
   {/if}
@@ -71,8 +45,11 @@ style:width={boxWidth} style:height={boxHeight}>
 .icon-box {
   @apply relative p-1 cursor-pointer flex justify-center items-center transition-colors bg-[var(--backgroundColor)] hover:bg-[var(--backgroundHover)] mb-3 rounded-[var(--borderRadius)] hover:rounded-[var(--borderRadiusHover)];
   transition: .2s;
+  &:hover &__tooltip {
+    @apply block;
+  }
   &__tooltip {
-    @apply absolute top-0 left-[140%] bg-black p-2 text-white rounded-md font-semibold transition-opacity;
+    @apply hidden absolute top-0 left-[140%] bg-black p-2 text-white rounded-md font-semibold transition-opacity;
     transition: .6s;
   }
 }
--- src/types/Icons.ts
+++ src/types/Icons.ts
@@ -1,27 +1,10 @@
 interface IPathIcon {
   d: string;
   fill?: string;
-  fillRule?: 'inherit' | 'nonzero' | 'evenodd' | null | undefined;
-  clipRule?: string;
-  stroke?: string;
-  strokeWidth?: string;
-  strokeLineCap?: 'inherit' | 'butt' | 'round' | 'square' | null | undefined;
-  strokeLineJoin?: 'inherit' | 'round' | 'miter' | 'bevel' | null | undefined;
-  transform?: string;
-  id?: string;
 }
 
 interface IIcon {
   viewBox: string;
   fill: string;
-  width?: string;
-  height?: string;
-  stroke?: string;
-  strokeWidth?: string;
-  g?: {
-    style?: string;
-    transform?: string;
-    id?: string;
-  };
   path: IPathIcon[];
 }
